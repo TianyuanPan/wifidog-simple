64d7e075c73b4d8c88e37b25faa00ccc11551474
TianyuanPan <TianyuanPan@localhost.localdomain>
2016-03-04 13:20:38 +0800

    fix gw_id get BUG

--- src/gateway.c
+++ src/gateway.c
@@ -385,8 +385,8 @@ main_loop(void)
      * "Can't fail" so exit() if the impossible happens. */
     if (!config->gw_id) {
         debug(LOG_DEBUG, "Finding MAC address of %s", config->gw_interface);
-        if ((config->gw_id = get_iface_mac(config->gw_interface)) == NULL) {
-            debug(LOG_ERR, "Could not get MAC address information of %s, exiting...", config->gw_interface);
+        if ((config->gw_id = get_iface_mac(config->external_interface)) == NULL) {
+            debug(LOG_ERR, "Could not get MAC address information of %s, exiting...", config->external_interface);
             exit(1);
         }
         debug(LOG_DEBUG, "%s = %s", config->gw_interface, config->gw_id);
